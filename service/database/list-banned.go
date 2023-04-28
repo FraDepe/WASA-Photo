518bde5dcb68907d316c5bb3c707721003127673
Fradepe <depersio.1938022@studenti.uniroma1.it>
2023-04-28 09:58:41 +0200

    Fixed query

--- service/database/list-banned.go
+++ service/database/list-banned.go
@@ -4,7 +4,7 @@ func (db *appdbimpl) ListBanned(loggedUser uint64) ([]User, error) {
 	var user_list []User
 
 	rows, err := db.c.Query(`	SELECT u.id, u.username, u.follower, u.following, u.banned, u.photos  
-								FROM users u, follows f
+								FROM users u, bans b
 								WHERE b.userid=? and b.bannedid=u.id`, loggedUser)
 	if err != nil {
 		return nil, err
