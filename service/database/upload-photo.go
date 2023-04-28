1f2d724d42632b376330401cc9008374e00a8aff
Fradepe <depersio.1938022@studenti.uniroma1.it>
2023-04-28 09:20:05 +0200

    Fixed typo

--- service/database/upload-photo.go
+++ service/database/upload-photo.go
@@ -2,7 +2,7 @@ package database
 
 func (db *appdbimpl) UploadPhoto(p Photo) (Photo, error) {
 
-	_, err := db.c.Exec(`INSERT INTO photos (id, userid, picture, like, date_time, comments) VALUES (NULL, ?, ?, ?, ?, ?)`,
+	_, err := db.c.Exec(`INSERT INTO photos (id, userid, picture, likes, date_time, comments) VALUES (NULL, ?, ?, ?, ?, ?)`,
 		p.Comments, p.Picture, p.Likes, p.Date_time, p.Comments)
 
 	if err != nil {
