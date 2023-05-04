156c862e189196b8dbb4f512168d1f703f39465d
Fradepe <depersio.1938022@studenti.uniroma1.it>
2023-05-04 21:02:08 +0200

    Fixed upload

--- service/database/upload-photo.go
+++ service/database/upload-photo.go
@@ -2,7 +2,7 @@ package database
 
 func (db *appdbimpl) UploadPhoto(p Photo) (Photo, error) {
 
-	_, err := db.c.Exec(`INSERT INTO photos (id, userid, picture, likes, date_time, comments) VALUES (NULL, ?, ?, ?, ?, ?)`,
+	res, err := db.c.Exec(`INSERT INTO photos (id, userid, picture, likes, date_time, comments) VALUES (NULL, ?, ?, ?, ?, ?)`,
 		p.Comments, p.Picture, p.Likes, p.Date_time, p.Comments)
 	if err != nil {
 		return p, err
@@ -13,6 +13,13 @@ func (db *appdbimpl) UploadPhoto(p Photo) (Photo, error) {
 	if err != nil {
 		return p, err
 	}
+
+	photo_id, err := res.LastInsertId()
+	if err != nil {
+		return p, err
+	}
+
+	p.ID = uint64(photo_id)
 
 	return p, nil
 }
