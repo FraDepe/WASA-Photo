9bb711b85c7a58603938c57c434c87bf404150c9
Fradepe <depersio.1938022@studenti.uniroma1.it>
2023-05-07 18:03:58 +0200

    Added sorting to getMyStream

--- service/database/get-my-stream.go
+++ service/database/get-my-stream.go
@@ -4,7 +4,7 @@ func (db *appdbimpl) GetMyStream(user uint64) ([]Photo, error) {
 	var stream_photo []Photo
 
 	rows, err := db.c.Query(`SELECT id, userid, picture, likes, date_time, comments FROM photos p, follows f 
-							 WHERE ?=f.followerid and f.followedid=p.userid`, user)
+							 WHERE ?=f.followerid and f.followedid=p.userid ORDER BY id DESC`, user)
 	if err != nil {
 		return nil, err
 	}
